68deafbdaa9080e187252a84bb16c0085acbc5ec
AKHIL RANA <44070118+axelakhil@users.noreply.github.com>
2019-10-18 01:31:10 +0530

    Update passdata.py

--- passdata.py
+++ passdata.py
@@ -6,34 +6,31 @@ mycursor = mydb.cursor()
 #######   ALREADY COMMITED DATA  #####################################
 
 # self.mycursor.execute("Create TABLE pass(Username VARCHAR(255),Password VARCHAR(255))")
-# self.mycursor.execute("INSERT INTO pass VALUES('AKHIL RANA','SUDO')")
+# self.mycursor.execute("INSERT INTO pass VALUES('AKHIL RANA','SUDO')")ru
 # self.mydb.commit()
 
-#GLOBAL VARIABLES FOR USERID AND PASSWORD
-
-username=""
-passcode=""
 
 #######READ FUNCTION TO CHECK THE VALIDITY OF ENTERED CREDENTIALS##############################################################
 
 def read(un,passw):
-# GETTING UN ,PASSW FROM LOGIN PAGE ENTRIES
-    global passcode
-    global username
-    # CODE TO CHECK WHETEHR CREDENTIALS EXIST IN THE SYSTEM OR NOT
-    try:
+#verifying the login details
         mycursor.execute("SELECT * FROM pass WHERE Username=%s and Password=%s", (un, passw))
-        for x in mycursor:
-            u = list(x)
-        #RETURNING AND ASSIGNING THE CREDENTIALS AFTER VERIFICATION FROM DATABASE
-        username = u[0]
-        passcode = u[1]
+        b=mycursor.fetchall()
+        if(b==[]):
+            return False
+        else:
+            return True
+
+def signup(un,passw,ms):
+    mycursor.execute("select * from pass where Username=%s",(un,))
+    b=mycursor.fetchall()
+
+    if(b==[] and ms=='123456A'):
+        mycursor.execute("INSERT INTO pass VALUES(%s,%s)", (un,passw,))
+        mydb.commit()
         return True
-    except:
-        #IF CREDENTIALS DON'T MATCH WITH THE DATABASE COMBINATION
+    else:
         return False
-
-
 
 
 
